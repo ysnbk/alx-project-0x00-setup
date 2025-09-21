0e11c2aa02550905fa8f2c128028eae96785373e
ysnbk <130864850+ysnbk@users.noreply.github.com>
2025-09-21 16:14:19 +0000

    second commit

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -11,7 +11,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export default function Home() {
+ const Home:React.FC = () => {
   return (
     <div
       className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
@@ -113,3 +113,4 @@ export default function Home() {
     </div>
   );
 }
+export default Home;
